86a6470b12b9c86d14564253bd93c4da8fa2d1e2
Oliver Turner <oliver.turner@codedsignal.co.uk>
2015-08-26 23:02:55 +0100

    Added sourcemap

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,54 +3,60 @@
  */
 
 /* globals jQuery, document */
-(function ($, undefined) {
-    "use strict";
+(function ($) {
+  'use strict';
 
-    var $document = $(document);
+  var $document, $htmlBody;
 
-    $document.ready(function () {
+  $htmlBody = $('html, body');
+  $document = $(document);
 
-        var $postContent = $(".post-content");
-        $postContent.fitVids();
+  $document.ready(function () {
+    var $postContent = $('.post-content');
+    $postContent.fitVids();
 
-        $(".scroll-down").arctic_scroll();
+    $('.scroll-down').arctic_scroll();
 
-        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
-            e.preventDefault();
-            $("body").toggleClass("nav-opened nav-closed");
-        });
-
+    $('.menu-button, .nav-cover, .nav-close').on('click', function (e) {
+      e.preventDefault();
+      $('body').toggleClass('nav-opened nav-closed');
     });
 
-    // Arctic Scroll by Paul Adam Davis
-    // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
+  });
 
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        },
+  // Arctic Scroll by Paul Adam Davis
+  // https://github.com/PaulAdamDavis/Arctic-Scroll
+  $.fn.arctic_scroll = function (options) {
+    var defaults, allOptions;
 
-        allOptions = $.extend(defaults, options);
-
-        allOptions.elem.click(function (event) {
-            event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-                toMove;
-
-            if (offset) {
-                toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
-            } else if (position) {
-                toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
-            } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
-            }
-        });
-
+    defaults = {
+      elem:  $(this),
+      speed: 500
     };
+
+    allOptions = $.extend(defaults, options);
+
+    allOptions.elem.click(function (event) {
+      var $this, offset, position, toMove;
+
+      event.preventDefault();
+
+      $this    = $(this);
+      offset   = ($this.attr('data-offset')) ? $this.attr('data-offset') : false;
+      position = ($this.attr('data-position')) ? $this.attr('data-position') : false;
+
+      if (offset) {
+        toMove = parseInt(offset, 10);
+        $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove)}, allOptions.speed);
+      }
+      else if (position) {
+        toMove = parseInt(position, 10);
+        $htmlBody.stop(true, false).animate({scrollTop: toMove}, allOptions.speed);
+      }
+      else {
+        $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top)}, allOptions.speed);
+      }
+    });
+
+  };
 })(jQuery);
